e5ffb6745578bb5e21b76e7ac15674bf992a21d2
sunmaolin <Mr.sun727@iCloud.com>
2020-04-23 21:45:16 +0800

    冒泡排序

---
+++ src/sml/sort/BubbleSort.java
@@ -0,0 +1,38 @@
+package sml.sort;
+
+import java.util.Arrays;
+
+/**
+ * 冒泡排序
+ * 描述：
+ *  1、比较相邻的元素，如果第一个比第二个大，就交换他们俩个
+ *  2、对每对相邻的元素做同样的工作，从开始第一对到结尾的最后一队，这样在最后的元素应该会是最大数
+ *  3、针对所有元素重复以上步骤，除了最后一个
+ *  4、重复步骤1-3，直到排序完成
+ */
+public class BubbleSort {
+
+    public static void sort(int[] array){
+
+        //临时变量，交换时用作中间体
+        int temp = 0;
+
+        if (array.length == 0)
+            System.out.println("空数组");
+        for (int i = 0; i < array.length; i++) {
+            //循环出最大值与最后一个数不需要比较
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if(array[j] > array[j+1]){
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
+
+
+    }
+
+}
---
+++ src/sml/sort/SortTest.java
@@ -0,0 +1,16 @@
+package sml.sort;
+
+public class SortTest {
+
+    public static void main(String[] args) {
+
+        int[] array = {2,5,1,14,32,12,68,49,6};
+
+        //测试冒泡排序
+        BubbleSort.sort(array);
+
+
+
+
+    }
+}
